53b5b38b512d427bdfc896ff87383811c27649b1
karishmask46 <karishmask.15999@gmail.com>
2022-08-13 16:37:39 +0530

    I am in a Happy mood

--- MoodAnalyser/MoodAnalyserUnitTest/UnitTest1.cs
+++ MoodAnalyser/MoodAnalyserUnitTest/UnitTest1.cs
@@ -6,12 +6,12 @@ namespace MoodAnalyserUnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void GivenSadMoodMessage_WhenAnalyseShouldReturnSad()
+        public void GivenAnyMoodMessage_WhenAnalyseShouldReturnHappy()
         {
             //Arrange
             MoodAnalyser1 analyser = new MoodAnalyser1();
-            string message = "I am in Sad Mood";
-            string expected = "SAD";
+            string message = "I am in Any Mood";
+            string expected = "HAPPY";
 
             //Act
             string actual = analyser.analyseMood(message);
