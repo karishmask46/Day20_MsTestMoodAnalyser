c3b155e090e5a39a6bf0c855907acd0d1175890d
karishmask46 <karishmask.15999@gmail.com>
2022-08-13 16:51:54 +0530

    Mood message

--- MoodAnalyser/MoodAnalyserUnitTest/UnitTest1.cs
+++ MoodAnalyser/MoodAnalyserUnitTest/UnitTest1.cs
@@ -5,6 +5,7 @@ namespace MoodAnalyserUnitTest
     [TestClass]
     public class UnitTest1
     {
+        [TestMethod]
         public void GivenSadMoodMessage_WhenAnalyseShouldReturnSad()
         {
             //Arrange
@@ -19,21 +20,7 @@ namespace MoodAnalyserUnitTest
             Assert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
-        public void GivenAnyMoodMessage_WhenAnalyseShouldReturnHappy()
-        {
-            //Arrange
-            string message = "I am in Any Mood";
-            MoodAnalyser1 analyser = new MoodAnalyser1(message);
-            string expected = "HAPPY";
-
-            //Act
-            string actual = analyser.analyseMood();
-
-            //Assert
-            Assert.AreEqual(expected, actual);
-        }
-
+       
     }
 
 
